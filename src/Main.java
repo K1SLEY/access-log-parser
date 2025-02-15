e7e674f836d2c624e3de319de3b2f180a794aecb
Anton Kiselev <kiselevas859999@gmail.com>
2025-02-15 13:25:18 +0300

    добавление логика для расчета дÐиух вводимых чисел

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+        System.out.println("Введите первое число:");
+        int number_1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number_2 = new Scanner(System.in).nextInt();
+        int sum = number_1 + number_2;
+        int subsum = number_1 - number_2;
+        int mult = number_1 * number_2;
+        double div =(double)number_1 / number_2;
+        System.out.println("Сумма чисел " + number_1 + " + "+ number_2 + " = " + sum  );
+        System.out.println("Разность чисел " + number_1 + " - "+ number_2 + " = " + subsum);
+        System.out.println("Умножение чисел " + number_1 + " * "+ number_2 + " = " + mult);
+        System.out.println("Частное чисел " + number_1 + " / "+ number_2 + " = " + div);
+
+
     }
 }
