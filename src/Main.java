dd9e468862534664ecb4284e8af987b44ef9cd1f
Anton Kiselev <kiselevas859999@gmail.com>
2025-02-25 21:01:19 +0300

    Добовление логики дÐая ввода пути к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " +
-                Math.random());
+
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+
+        while (true) {
+            System.out.println("Введите путь к файлу");
+            String path = scanner.nextLine();
+            File file = new File(path);
+            boolean isFile = file.isFile();
+
+            if (!isFile) {
+                if (!file.exists()) {
+                    System.out.println("Файла не существует.");
+                } else {
+                    System.out.println("Указанный путь является директорией.");
+                }
+                continue;
+            }
+
+            System.out.println("Путь указан верно");
+            count++;
+            System.out.println("Это файл номер " + count);
+        }
     }
 }
+
