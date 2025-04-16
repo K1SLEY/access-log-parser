1663ea173379cfe021b640be5ff51a21c7c7b0b5
Anton Kiselev <kiselevas859999@gmail.com>
2025-04-16 22:14:02 +0300

    add browser and 404

--- src/Main.java
+++ src/Main.java
@@ -62,6 +62,13 @@ public class Main {
                 osStats.forEach((os, ratio) ->
                         System.out.printf("%s: %.2f%%\n", os, ratio * 100));
 
+
+                HashMap<String, Double> browserStats = stats.getBrowserStats();
+                System.out.println("Cтраницы с ошибкой 404: ");
+                stats.getNotFoundPages().forEach(page -> System.out.println("-! " + page));
+                browserStats.forEach((browser, ratio) ->
+                        System.out.printf("%s: %.2f%%\n", browser, ratio * 100));
+
                 System.out.println("Общее количество запросов: " + totalRequests);
 
                 System.out.printf("Средний трафик в час: %.2f мб/час%n", stats.getTrafficRate() / 1048576);
--- src/Statistics.java
+++ src/Statistics.java
@@ -9,7 +9,8 @@ class Statistics {
     private LocalDateTime maxTime;
     private final HashSet<String> existingPages = new HashSet<>();
     private final HashMap<String, Integer> osStats = new HashMap<>();
-
+    private final HashSet<String> notFoundPages = new HashSet<>();
+    private final HashMap<String, Integer> browserStats = new HashMap<>();
 
     public void addEntry(LogEntry entry) {
         if (entry.getDataSize() > 0) {
@@ -23,15 +24,36 @@ class Statistics {
         if (maxTime == null || entry.getDateTime().isAfter(maxTime)) {
             maxTime = entry.getDateTime();
         }
+        if (entry.getResponseCode() == 404)
+            notFoundPages.add(entry.getPath());
+
         if (entry.getResponseCode() == 200) {
             existingPages.add(entry.getPath());
         }
         String os = entry.getUserAgent().getOs();
         osStats.put(os, osStats.getOrDefault(os, 0) + 1);
+
+        String browser = entry.getUserAgent().getBrowser();
+        browserStats.put(browser, browserStats.getOrDefault(os, 0) + 1);
     }
 
     public HashSet<String> getExistingPages() {
         return new HashSet<>(existingPages);
+    }
+
+    public HashSet<String> getNotFoundPages() {
+        return new HashSet<>(notFoundPages);
+    }
+
+    public HashMap<String, Double> getBrowserStats() {
+        HashMap<String, Double> resultBrowser = new HashMap<>();
+        int totalRequests = browserStats.values().stream().mapToInt(Integer::intValue).sum();
+
+        if (totalRequests == 0) return resultBrowser;
+
+        double totalRequestsDouble = (double) totalRequests;
+        browserStats.forEach((browser, count) -> resultBrowser.put(browser, (double) count / totalRequestsDouble));
+        return resultBrowser;
     }
 
     public HashMap<String, Double> getOsStatistics() {
